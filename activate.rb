07e9edd651254e77ea99ed4a8d6400545e7442de
abrahamsangha <abraham.sangha@gmail.com>
2014-05-22 20:40:54 -0500

    Added output messages

--- activate.rb
+++ activate.rb
@@ -17,5 +17,7 @@ dot_files.each do |filename|
   sym_link = File.join(home_dir, ".#{File.basename(filename)}")
 
   FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
+  str = "In #{home_dir}, creating symlink .#{File.basename(filename)} to #{filename}"
+  puts "\e[#{32}m#{str}\e[0m"
   FileUtils.ln_s filename, sym_link
 end
